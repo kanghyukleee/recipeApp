a326ba939e29d36855cbfd4f4f66750579e79eb8
ViVaceLoca <movidic9656@gmail.com>
2023-08-02 01:36:15 +0900

    add login route with google OAuth2

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,23 @@
-<h1>
-  Test h1
-</h1>
+<div class="test-bg">
+  <h1>
+    Test h1
+  </h1>
+  <p>
+    text
+    <a>anchor tag</a>
+    another text
+  </p>
+  <input />
+  <button>button</button>
+</div>
 <p>some random texts</p>
 
+<style lang="scss">
+  .test-bg {
+    padding: 50px;
+    // background-color: var(--light-bg-color);
+  }
+  h1 {
+    color: var(--accent-color)
+  }
+</style>
---
+++ src/routes/api/auth/login/+server.ts
@@ -0,0 +1,50 @@
+import { error, redirect } from '@sveltejs/kit';
+import type { RequestHandler } from '../../../login/oauth2/code/google/$types';
+import {
+	GOOGLE_APP_CLIENT_ID,
+	GOOGLE_APP_CLIENT_SECRET,
+	GOOGLE_REDIRECTION_URI
+} from '$env/static/private';
+
+export const GET: RequestHandler = async ({ url, cookies, fetch }) => {
+	const code = url.searchParams.get('code') || null;
+	const state = url.searchParams.get('state') || null;
+
+	const storedState = cookies.get('google_auth_state') || null;
+	const storedChallengeVerifier = cookies.get('google_auth_challenge_verifier') || null;
+
+	if (state === null || state !== storedState) {
+		throw error(400, 'State Mismatch!');
+	}
+
+	const response = await fetch('https://oauth2.googleapis.com/token', {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/x-www-form-urlencoded',
+			Authorization: `Basic ${Buffer.from(
+				`${GOOGLE_APP_CLIENT_ID}:${GOOGLE_APP_CLIENT_SECRET}`
+			).toString('base64')}`
+		},
+		body: new URLSearchParams({
+			code: code || '',
+			redirect_uri: GOOGLE_REDIRECTION_URI,
+			grant_type: 'authorization_code',
+            code_verifier: storedChallengeVerifier || '',
+            client_id: GOOGLE_APP_CLIENT_ID,
+		})
+	});
+    const responseJSON = await response.json();
+
+    if(responseJSON.error) {
+        throw error(400, responseJSON.error_description)
+    }
+    
+    cookies.delete('google_auth_state')
+    cookies.delete('google_auth_challenge_verifier')
+    cookies.set('access_token', responseJSON.access_token, { path: '/' });
+    cookies.set('id_token', responseJSON.id_token, {path: '/'})
+
+    throw redirect(303, '/');
+  
+    
+};
