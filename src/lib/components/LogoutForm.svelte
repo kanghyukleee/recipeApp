75799a9c435077110be484ba6c77f28dab5ccf2c
ViVaceLoca <movidic9656@gmail.com>
2023-08-05 03:00:52 +0900

    add LogoutForm component

---
+++ src/lib/components/LogoutForm.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import { invalidateAll } from '$app/navigation';
+</script>
+
+<form
+	method="POST"
+	action="/api/auth/logout"
+	on:submit|preventDefault={async () => {
+		const res = await fetch('/api/auth/logout', {
+			method: 'POST',
+			headers: {
+				accept: 'application/json'
+			}
+		});
+		if (res.ok) {
+			invalidateAll();
+		}
+	}}
+>
+	<button type="submit">logout</button>
+</form>
