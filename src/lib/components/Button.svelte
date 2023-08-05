209b53f4666a9c02d1b2f06a68560813a789b239
ViVaceLoca <movidic9656@gmail.com>
2023-08-05 23:22:46 +0900

    enhance type definition on Button component

--- src/lib/components/Button.svelte
+++ src/lib/components/Button.svelte
@@ -1,4 +1,19 @@
 <script lang="ts">
+	import type { HTMLAnchorAttributes, HTMLButtonAttributes } from 'svelte/elements';
+
+	type Element = $$Generic<'button' | 'a'>;
+
+	interface ButtonComponentElements {
+		button: HTMLButtonAttributes;
+		a : HTMLAnchorAttributes;
+	}
+
+	type $$Props = ButtonComponentElements[Element] & {
+		element: Element;
+		varient?: 'filled' | 'outline' | 'danger';
+		className?: string;
+	}; 
+	
 	export let element: 'button' | 'a';
 	export let varient: 'filled' | 'outline' | 'danger' = 'filled';
 	export let className: string = '';
@@ -48,22 +63,24 @@
 			cursor: not-allowed;
 		}
 		&:hover {
-			&.button-filled, &.button-danger {
+			&.button-filled,
+			&.button-danger {
 				background-image: linear-gradient(rgba(0, 0, 0, 0.2) 0 0);
-        border-image: linear-gradient(rgba(0, 0, 0, 0.2) 0 0);
+				border-image: linear-gradient(rgba(0, 0, 0, 0.2) 0 0);
 			}
-      &.button-outline {
-        background-image: linear-gradient(rgba(255, 255, 255, 0.2) 0 0);
-      }
-		}
-    &:active {
-      &.button-filled, &.button-danger {
+			&.button-outline {
 				background-image: linear-gradient(rgba(255, 255, 255, 0.2) 0 0);
-        border-image: linear-gradient(rgba(255, 255, 255, 0.2) 0 0);
 			}
-      &.button-outline {
-        background-image: linear-gradient(rgba(255, 255, 255, 0.4) 0 0);
-      }
-    }
+		}
+		&:active {
+			&.button-filled,
+			&.button-danger {
+				background-image: linear-gradient(rgba(255, 255, 255, 0.2) 0 0);
+				border-image: linear-gradient(rgba(255, 255, 255, 0.2) 0 0);
+			}
+			&.button-outline {
+				background-image: linear-gradient(rgba(255, 255, 255, 0.4) 0 0);
+			}
+		}
 	}
 </style>
