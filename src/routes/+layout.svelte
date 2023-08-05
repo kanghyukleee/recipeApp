f1a68b5eeea93280cde1affbfda52cc327948213
ViVaceLoca <movidic9656@gmail.com>
2023-08-05 20:42:20 +0900

    add assets(logo, icons)

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,15 +3,17 @@
 	import '../styles/main.scss';
 	import type { LayoutData } from './$types';
 	import { LogoutForm } from '$components';
+	import  logoNoBackground from '$assets/logo-no-background.png'
 
 	export let data: LayoutData;
 
 	$: user = data.user;
 </script>
+<img src="{logoNoBackground}" alt="The Recipe">
 
 {#if user}
 	<p>Hello, {data.user.given_name}</p>
-    <LogoutForm />
+	<LogoutForm />
 {/if}
 
 <slot />
