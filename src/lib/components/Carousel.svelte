4cacb53aec70da393908c98b3276263823e0041d
ViVaceLoca <movidic9656@gmail.com>
2023-08-06 03:04:42 +0900

    add Carousel components

---
+++ src/lib/components/Carousel.svelte
@@ -0,0 +1,56 @@
+<script lang="ts">
+	import Siema from 'siema';
+	import { onMount } from 'svelte';
+
+	export let perPage:number = 1;
+	export let loop:boolean = true;
+	export let autoplay:number = 0;
+	export let duration:number = 500;
+	export let easing:string = 'ease-out';
+	export let startIndex:number = 0;
+	export let draggable:boolean = true;
+	export let multipleDrag:boolean = true;
+	export let threshold:number = 20;
+	export let rtl:boolean = false;
+
+	let controller: Siema;
+  let timer: NodeJS.Timer;
+
+	onMount(() => {
+		controller = new Siema({
+			selector: '.slides',
+			duration,
+			easing,
+			// perPage: typeof perPage === 'object' ? perPage : Number(perPage),
+			startIndex,
+			draggable,
+			multipleDrag,
+			loop,
+			threshold,
+			rtl
+		});
+    if(autoplay > 0){
+
+      timer = setInterval(()=>{controller.next()}, autoplay)
+    }
+    return  () => {
+      autoplay && clearInterval(timer)
+      controller.destroy()
+    }
+	});
+</script>
+
+<div class="carousel">
+	<div class="slides">
+		<slot />
+	</div>
+</div>
+
+<style lang="scss">
+	.carousel {
+		position: relative;
+		width: 100%;
+		justify-content: center;
+		align-items: center;
+	}
+</style>
--- src/lib/components/index.ts
+++ src/lib/components/index.ts
@@ -1,2 +1,3 @@
 export { default as LogoutForm } from './LogoutForm.svelte';
 export { default as Button } from './Button.svelte';
+export { default as Carousel } from './Carousel.svelte';
