d0d92bbd11e209cac651b0e7d0d2049370264c92
ViVaceLoca <movidic9656@gmail.com>
2023-08-08 00:52:39 +0900

    rollback Carousel codes

--- src/lib/components/Carousel.svelte
+++ src/lib/components/Carousel.svelte
@@ -1,20 +1,20 @@
 <script lang="ts">
-	import Siema from 'siema';
+	import Siema from 'siema'
 	import { onMount } from 'svelte';
 
-	export let perPage:number = 1;
-	export let loop:boolean = true;
-	export let autoplay:number = 0;
-	export let duration:number = 500;
-	export let easing:string = 'ease-out';
-	export let startIndex:number = 0;
-	export let draggable:boolean = true;
-	export let multipleDrag:boolean = true;
-	export let threshold:number = 20;
-	export let rtl:boolean = false;
+	export let perPage: number = 1;
+	export let loop: boolean = true;
+	export let autoplay: number = 0;
+	export let duration: number = 500;
+	export let easing: string = 'ease-out';
+	export let startIndex: number = 0;
+	export let draggable: boolean = true;
+	export let multipleDrag: boolean = true;
+	export let threshold: number = 20;
+	export let rtl: boolean = false;
 
 	let controller: Siema;
-  let timer: NodeJS.Timer;
+	let timer: NodeJS.Timer;
 
 	onMount(() => {
 		controller = new Siema({
@@ -29,14 +29,15 @@
 			threshold,
 			rtl
 		});
-    if(autoplay > 0){
-
-      timer = setInterval(()=>{controller.next()}, autoplay)
-    }
-    return  () => {
-      autoplay && clearInterval(timer)
-      controller.destroy()
-    }
+		if (autoplay > 0) {
+			timer = setInterval(() => {
+				controller.next();
+			}, autoplay);
+		}
+		return () => {
+			autoplay && clearInterval(timer);
+			controller.destroy();
+		};
 	});
 </script>
 
