b81a86817f9d8b71e90ae90062bf0fae934d9d55
ViVaceLoca <movidic9656@gmail.com>
2023-08-08 00:04:05 +0900

    add Toggle switch components

---
+++ src/lib/components/ToggleSwitch.svelte
@@ -0,0 +1,45 @@
+
+
+<label class="switch">
+  <input class='switch-icon' type="checkbox" role="switch">
+  <span><slot /></span>
+</label>
+
+
+<style lang="scss">
+  .switch {
+    display: inline-flex;
+    align-items: center;
+    gap: 0.5rem;
+    margin: 10px;
+    cursor: pointer;
+    .switch-icon {
+      appearance: none;
+      position: relative;
+      border: max(2px, 0.1em) solid gray;
+      border-radius: 1.25em;
+      width: 2.25em;
+      height: 1.25em;
+    }
+    [type="checkbox"]::before {
+      content: "";
+      position: absolute;
+      left: 0;
+      width: 1em;
+      height: 1em;
+      border-radius: 50%;
+      transform: scale(0.8);
+      background-color: gray;
+      transition: left 120ms linear;
+    }
+    [type="checkbox"]:checked::before {
+      background-color: white;
+      left:1em;
+    }
+    [type="checkbox"]:checked {
+      background-color: var(--accent-color);
+      border-color: var(--accent-color);
+    }
+    
+  }
+</style>
--- src/lib/components/index.ts
+++ src/lib/components/index.ts
@@ -1,3 +1,5 @@
 export { default as LogoutForm } from './LogoutForm.svelte';
 export { default as Button } from './Button.svelte';
 export { default as Carousel } from './Carousel.svelte';
+export { default as Navigation } from './Navigation.svelte';
+export { default as ToggleSwitch } from './ToggleSwitch.svelte';
