62d71c858932c710c2f2c6a1f61cfd10702253d7
ViVaceLoca <movidic9656@gmail.com>
2023-09-14 01:41:48 +0900

    homepage api call setting

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,9 +1,9 @@
 import type { PageLoad } from "./$types";
 
 export const load: PageLoad = async ({fetch}) => {
-  const recipe = await fetch('/api/recipe?limit=4');
+  const recipe = await fetch('/api/recipe?limit=12');
   const recipeRes = await recipe.json();  
-  
+
   return {
     recipe: recipeRes
   }
