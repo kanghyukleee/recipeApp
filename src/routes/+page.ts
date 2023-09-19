97ab67633097723e687cfe5612c11b62f1a0e334
ViVaceLoca <movidic9656@gmail.com>
2023-09-20 01:54:56 +0900

    adopt fetch refresh function

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,10 +1,19 @@
-import type { PageLoad } from "./$types";
+import { fetchRefresh } from '$helpers';
+import type { PageLoad } from './$types';
 
-export const load: PageLoad = async ({fetch}) => {
-  const recipe = await fetch('/api/recipe?limit=12');
-  const recipeRes = await recipe.json();  
+// auto refresh token 
+// import { fetchRefresh } from "$helpers";
 
-  return {
-    recipe: recipeRes
-  }
-}
+export const load: PageLoad = async ({ fetch: _fetch }) => {
+	// auto refresh token 
+	// const fetch = (path:string) => fetchRefresh(_fetch, path)  // fetch: _fetch
+
+	const fetch = (path: string) => fetchRefresh(_fetch, path);
+
+	const recipe = await fetch('/api/recipe?limit=12');
+	const recipeRes = await recipe.json();
+
+	return {
+		recipe: recipeRes
+	};
+};
