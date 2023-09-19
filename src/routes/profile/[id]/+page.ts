03124c2f202af35251d12eb76998be77315af17b
ViVaceLoca <movidic9656@gmail.com>
2023-09-20 01:55:17 +0900

    profile route setup

---
+++ src/routes/profile/[id]/+page.ts
@@ -0,0 +1,19 @@
+import { fetchRefresh } from "$helpers";
+import type { error } from "@sveltejs/kit";
+import type { PageLoad } from "./$types";
+
+export const load: PageLoad = async ({fetch: _fetch, params}) => {
+  const fetch = (path: string) => fetchRefresh(_fetch, path);
+  
+  const profileRes = await fetch(`/api/profile/${params.id}`);
+
+  // if{!profileRes.ok} {
+  //   throw error(profileRes.status, 'Failed to load profile!');
+  // }
+
+  const profileJSON = await profileRes.json()
+
+  return {
+    profile: profileJSON
+  }
+}
