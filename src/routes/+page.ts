2ffe30f8c5094445ca2a6397b578c2631e33181d
ViVaceLoca <movidic9656@gmail.com>
2023-09-23 22:58:45 +0900

    load data without login

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,14 +1,13 @@
-import { fetchRefresh } from '$helpers';
+// import { fetchRefresh } from '$helpers';
 import type { PageLoad } from './$types';
 
 // auto refresh token 
 // import { fetchRefresh } from "$helpers";
 
-export const load: PageLoad = async ({ fetch: _fetch }) => {
+export const load: PageLoad = async ({ fetch }) => {
 	// auto refresh token 
 	// const fetch = (path:string) => fetchRefresh(_fetch, path)  // fetch: _fetch
 
-	const fetch = (path: string) => fetchRefresh(_fetch, path);
 
 	const recipe = await fetch('/api/recipe?limit=12');
 	const recipeRes = await recipe.json();
--- src/routes/profile/[id]/+page.ts
+++ src/routes/profile/[id]/+page.ts
@@ -2,8 +2,8 @@ import { fetchRefresh } from "$helpers";
 import type { error } from "@sveltejs/kit";
 import type { PageLoad } from "./$types";
 
-export const load: PageLoad = async ({fetch: _fetch, params}) => {
-  const fetch = (path: string) => fetchRefresh(_fetch, path);
+export const load: PageLoad = async ({fetch, params}) => {
+  // const fetch = (path: string) => fetchRefresh(_fetch, path); // fetch: _fetch
   
   const profileRes = await fetch(`/api/profile/${params.id}`);
 
