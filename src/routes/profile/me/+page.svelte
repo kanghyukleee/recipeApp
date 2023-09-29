284833b81f6d7bc87e8dba7022fdb97d56171911
ViVaceLoca <movidic9656@gmail.com>
2023-09-29 19:01:47 +0900

    basic setup for user profile

---
+++ src/routes/profile/me/+page.svelte
@@ -0,0 +1,68 @@
+<script lang="ts">
+	import { Card } from '$components';
+	import type { PageData } from './$types';
+
+	type RecipeType = {
+		_id: string;
+		type: 'recipe';
+		owner_id: string;
+		title: string;
+		image?: string;
+		rating: {
+			user_id: string;
+			rate: 0 | 1 | 2 | 3 | 4 | 5;
+			comment?: string;
+		}[];
+		categories: string[];
+		description: string;
+		prep_time: string;
+		cook_time: string;
+		yield: string;
+		ingredient: {
+			name: string;
+			quantity: string;
+			unit?: string;
+			note?: string;
+		}[];
+		steps: {
+			step_number: number;
+			instruction: string;
+			duration: string;
+			image?: string;
+		}[];
+	};
+	type ProfileType = {
+		_id: string;
+		email: string; // extracted from login
+		type: 'profile';
+		user_image: string; // extracted from login
+		name: string; // extracted from login
+		recipe_ids: string[];
+		followers: number;
+	};
+	export let data: PageData;
+
+	const profile:ProfileType = data.profile
+
+  
+	let recipes: RecipeType[] = []; // recipes to display. 4~8 recipes
+</script>
+
+<h1>User Profile</h1>
+<div class="info">
+  {profile.name}
+</div>
+
+
+<div class="recipe-lists">
+	<div class="header">
+		<div class="header-left" />
+		<div class="header-right">See All</div>
+	</div>
+	<div class="gird-items">
+    card content
+  </div>
+</div>
+<!-- invisible loading spin  -->
+<style lang="scss">
+</style>
--- src/routes/profile/me/+page.ts
+++ src/routes/profile/me/+page.ts
@@ -5,19 +5,21 @@ import type { PageLoad } from './$types';
 // login required
 export const load: PageLoad = async ({ fetch: _fetch, parent }) => {
 	const fetch = (path: string) => fetchRefresh(_fetch, path);
-  
-	const { user, userProfile } = await parent();  // this is user info given by google
+
+	const { user, userProfile } = await parent(); // this is user info given by google
 	if (!user) {
 		throw error(401, 'Session Expired!');
 	}
 	if (userProfile && userProfile._id) {
-
-		const profileRes = await fetch(`/api/profile/me?id=${userProfile._id}`);  
-		const profileJSON = await profileRes.json();
-
-		return {
-			profile: profileJSON
-		};
+		const profileRes = await fetch(`/api/profile/me?id=${userProfile._id}`);
+		if (profileRes.ok) {
+			const profileJSON = await profileRes.json();
+			return {
+				profile: profileJSON
+			};
+		} else {
+			throw error(404, 'Profile not Found!');
+		}
 	} else {
 		throw error(400, 'Profile not Found!');
 	}
