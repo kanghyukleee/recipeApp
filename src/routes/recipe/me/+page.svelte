ff58541722f8f55a1563043b409644b462b6fe6e
ViVaceLoca <movidic9656@gmail.com>
2023-10-01 04:12:10 +0900

    enhance my recipes route

--- src/routes/recipe/me/+page.svelte
+++ src/routes/recipe/me/+page.svelte
@@ -39,44 +39,36 @@
 	$: if (data.userRecipes.length > 0) {
 		recipes = data.userRecipes;
 	}
-
-	const followersFormat = Intl.NumberFormat('en', { notation: 'compact' });
 </script>
 
 {#if userProfile}
-	
-		<div class="content">
-			{#if recipes.length > 0}
-				<div class="grid-items">
-					{#each recipes as recipe}
-						<div class="grid-item">
-							<Card item={recipe} />
-						</div>
-					{/each}
+	{#if recipes.length > 0}
+		<div class="grid-items recipe">
+			{#each recipes as recipe}
+				<div class="grid-item">
+					<Card item={recipe} />
 				</div>
-			{:else}
-				<div class="item-placeholder">
-					<h1 class="notification">Upload Your Recipe</h1>
-					<Button element="a" href="/">Add Recipe</Button>
-				</div>
-			{/if}
+			{/each}
 		</div>
-
+	{:else}
+		<div class="item-placeholder">
+			<h1 class="notification">Upload Your Recipe</h1>
+			<Button element="a" href="/">Add Recipe</Button>
+		</div>
+	{/if}
 {:else}
 	<h1>Error!</h1>
 {/if}
 
 <style lang="scss">
-	.content {
-		.item-placeholder {
-			height: 100%;
-			align-items: center;
-			text-align: center;
-			margin-top: 100px;
-			.notification {
-				font-size: functions.toRem(52);
-				margin-bottom: 50px;
-			}
+	.item-placeholder {
+		height: 100%;
+		align-items: center;
+		text-align: center;
+		margin-top: 100px;
+		.notification {
+			font-size: functions.toRem(52);
+			margin-bottom: 50px;
 		}
 	}
 </style>
